14ef85a4bb4c76cb2f17159edcce997f56799c51
Vitalik Buterin <v@buterin.com>
2014-06-06 01:11:30 -0400

    whoops forgot to add files

---
+++ compiler.cpp
@@ -0,0 +1,269 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include "rewriter.cpp"
+#include "opcodes.cpp"
+
+struct programData {
+    std::map<std::string, std::string> vars;
+    bool allocUsed;
+    bool calldataUsed;
+    std::vector<Node> code;
+};
+
+programData pd() {
+    programData o;
+    o.allocUsed = false;
+    o.calldataUsed = false;
+    return o;
+}
+
+programData merge(programData existing, programData newdata) {
+    for (std::map<std::string,std::string>::iterator it = newdata.vars.begin();
+         it != newdata.vars.end();
+         it++) {
+        existing.vars[(*it).first] = (*it).second;
+    }
+    existing.allocUsed = existing.allocUsed || newdata.allocUsed;
+    existing.calldataUsed = existing.calldataUsed || newdata.calldataUsed;
+    existing.code = newdata.code;
+    return existing;
+}
+
+programData compile(Node node, programData pdata=pd()) {
+    std::string symb = "_"+intToDecimal(counter);
+    counter += 1;
+    if (node.type == TOKEN) {
+        pdata.code.push_back(nodeToNumeric(node));
+    }
+    else if (node.val == "ref" || node.val == "get" || node.val == "set") {
+        std::string varname = node.args[0].val;
+        if (!pdata.vars.count(varname)) {
+            pdata.vars[varname] = intToDecimal(pdata.vars.size() * 32);
+        }
+        if (varname == "msg.data") pdata.calldataUsed = true;
+        if (node.val == "set") {
+            pdata = merge(pdata, compile(node.args[1], pdata));
+        }
+        pdata.code.push_back(token(pdata.vars[varname], node.metadata));
+        if (node.val == "set") {
+             pdata.code.push_back(token("MSTORE", node.metadata));
+        }
+        else if (node.val == "get") {
+             pdata.code.push_back(token("MLOAD", node.metadata));
+        }
+    }
+    else if (node.val == "seq") {
+        for (int i = 0; i < node.args.size(); i++) {
+            pdata = merge(pdata, compile(node.args[i], pdata));
+        }
+    }
+    else if (node.val == "unless" && node.args.size() == 2) {
+        pdata = merge(pdata, compile(node.args[0], pdata));
+        pdata.code.push_back(token("$endif"+symb, node.metadata));
+        pdata.code.push_back(token("JUMPI", node.metadata));
+        pdata = merge(pdata, compile(node.args[1], pdata));
+        pdata.code.push_back(token("~endif"+symb, node.metadata));
+    }
+    else if (node.val == "if" && node.args.size() == 3) {
+        pdata = merge(pdata, compile(node.args[0], pdata));
+        pdata.code.push_back(token("NOT", node.metadata));
+        pdata.code.push_back(token("$else"+symb, node.metadata));
+        pdata.code.push_back(token("JUMPI", node.metadata));
+        pdata = merge(pdata, compile(node.args[1], pdata));
+        pdata.code.push_back(token("$endif"+symb, node.metadata));
+        pdata.code.push_back(token("JUMP", node.metadata));
+        pdata.code.push_back(token("~else"+symb, node.metadata));
+        pdata = merge(pdata, compile(node.args[2], pdata));
+        pdata.code.push_back(token("~endif"+symb, node.metadata));
+    }
+    else if (node.val == "until") {
+        pdata.code.push_back(token("~beg"+symb, node.metadata));
+        pdata = merge(pdata, compile(node.args[0], pdata));
+        pdata.code.push_back(token("$end"+symb, node.metadata));
+        pdata.code.push_back(token("JUMPI", node.metadata));
+        pdata = merge(pdata, compile(node.args[1], pdata));
+        pdata.code.push_back(token("$beg"+symb, node.metadata));
+        pdata.code.push_back(token("JUMP", node.metadata));
+        pdata.code.push_back(token("~end"+symb, node.metadata));
+    }
+    else if (node.val == "lll") {
+        std::string LEN = "$begincode"+symb+".endcode"+symb;
+        pdata.code.push_back(token(LEN, node.metadata));
+        pdata.code.push_back(token("DUP", node.metadata));
+        pdata = merge(pdata, compile(node.args[1], pdata));
+        pdata.code.push_back(token("$begincode"+symb, node.metadata));
+        pdata.code.push_back(token("CODECOPY", node.metadata));
+        pdata.code.push_back(token("$endcode"+symb, node.metadata));
+        pdata.code.push_back(token("JUMP", node.metadata));
+        pdata.code.push_back(token("~begincode"+symb, node.metadata));
+        pdata.code.push_back(token("#CODE_BEGIN", node.metadata));
+        pdata = merge(pdata, compile(node.args[0], pdata));
+        pdata.code.push_back(token("#CODE_END", node.metadata));
+        pdata.code.push_back(token("~endcode"+symb, node.metadata));
+    }
+    else if (node.val == "alloc") {
+        pdata.allocUsed = true;
+        pdata = merge(pdata, compile(node.args[0], pdata));
+        pdata.code.push_back(token("MSIZE", node.metadata));
+        pdata.code.push_back(token("SWAP", node.metadata));
+        pdata.code.push_back(token("MSIZE", node.metadata));
+        pdata.code.push_back(token("ADD", node.metadata));
+        pdata.code.push_back(token("1", node.metadata));
+        pdata.code.push_back(token("SWAP", node.metadata));
+        pdata.code.push_back(token("SUB", node.metadata));
+        pdata.code.push_back(token("0", node.metadata));
+        pdata.code.push_back(token("SWAP", node.metadata));
+        pdata.code.push_back(token("MSTORE8", node.metadata));
+    }
+    else if (node.val == "array_lit") {
+        pdata.allocUsed = true;
+        pdata.code.push_back(token("MSIZE", node.metadata));
+        if (node.args.size()) {
+            pdata.code.push_back(token("DUP", node.metadata));
+            for (int i = 0; i < node.args.size(); i++) {
+                pdata = merge(pdata, compile(node.args[i], pdata));
+                pdata.code.push_back(token("SWAP", node.args[i].metadata));
+                pdata.code.push_back(token("MSTORE", node.args[i].metadata));
+                pdata.code.push_back(token("DUP", node.args[i].metadata));
+                pdata.code.push_back(token("32", node.args[i].metadata));
+                pdata.code.push_back(token("ADD", node.args[i].metadata));
+            }
+            pdata.code.pop_back();
+            pdata.code.pop_back();
+            pdata.code.pop_back();
+        }
+    }
+    else {
+        for (int i = node.args.size() - 1; i >= 0; i--) {
+            pdata = merge(pdata, compile(node.args[i], pdata));
+        }
+        pdata.code.push_back(token(node.val, node.metadata));
+    }
+    return pdata;
+}
+
+programData finalize(programData c) {
+    std::vector<Node> newCode;
+    if (c.allocUsed && c.vars.size() > 0) {
+        newCode.push_back(token("0", c.code[0].metadata));
+        std::string lastMem = intToDecimal(c.vars.size() * 32 - 1);
+        newCode.push_back(token(lastMem, c.code[0].metadata));
+        newCode.push_back(token("MSTORE8", c.code[0].metadata));
+    }
+    if (c.calldataUsed) {
+        newCode.push_back(token("MSIZE", c.code[0].metadata));
+        newCode.push_back(token("CALLDATASIZE", c.code[0].metadata));
+        newCode.push_back(token("MSIZE", c.code[0].metadata));
+        newCode.push_back(token("0", c.code[0].metadata));
+        newCode.push_back(token("CALLDATACOPY", c.code[0].metadata));
+        newCode.push_back(token(c.vars["msg.data"], c.code[0].metadata));
+        newCode.push_back(token("MSTORE", c.code[0].metadata));
+    }
+    for (int i = 0; i < c.code.size(); i++) newCode.push_back(c.code[i]);
+    c.code = newCode;
+    return c;
+}
+
+std::vector<Node> dereference(programData c) {
+    int labelLength = 1;
+    int tokenCount = c.code.size();
+    while (tokenCount > 64) {
+        tokenCount /= 256;
+        labelLength += 1;
+    }
+    std::vector<Node> iq;
+    for (int i = c.code.size() - 1; i >= 0; i--) iq.push_back(c.code[i]);
+    std::map<std::string,std::string> labelMap;
+    int pos = 0;
+    std::vector<int> codeStk;
+    codeStk.push_back(0);
+    std::vector<Node> mq;
+    while (iq.size()) {
+        Node front = iq.back();
+        iq.pop_back();
+        if (!isNumberLike(front) && front.val[0] == '~') {
+            labelMap[front.val.substr(1)] = intToDecimal(pos - codeStk.back());
+        }
+        else if (front.val == "#CODE_BEGIN") codeStk.push_back(pos);
+        else if (front.val == "#CODE_END") codeStk.pop_back();
+        else {
+            mq.push_back(front);
+            if (isNumberLike(front)) {
+                pos += 1 + (decimalLog256(front.val) || 1);
+            }
+            else if (front.val[0] == '$') {
+                pos += labelLength + 1;
+            }
+            else pos += 1;
+        }
+    }
+    std::vector<Node> oq;
+    for (int i = 0; i < mq.size(); i++) {
+        Node m = mq[i];
+        if (isNumberLike(m)) {
+            m = nodeToNumeric(m);
+            int len = decimalLog256(m.val) || 1;
+            oq.push_back(token("PUSH"+intToDecimal(len), m.metadata));
+            std::vector<Node> os = toByteArr(m.val, m.metadata);
+            for (int i = 0; i < os.size(); i++) oq.push_back(os[i]);
+        }
+        else if (m.val[0] == '$') {
+            std::vector<Node> os;
+            int dotLoc = m.val.find('.');
+            if (dotLoc == -1) {
+                std::string tokStr = "PUSH"+intToDecimal(labelLength);
+                oq.push_back(token(tokStr, m.metadata));
+                os = toByteArr(labelMap[m.val.substr(1)], m.metadata);
+            }
+            else {
+                std::string tokStr = "PUSH"+intToDecimal(labelLength);
+                oq.push_back(token(tokStr, m.metadata));
+                std::string start = labelMap[m.val.substr(1, dotLoc - 1)],
+                            end = labelMap[m.val.substr(dotLoc + 1)],
+                            dist = decimalSub(end, start);
+                std::cout << m.val.substr(1, dotLoc - 1) << " " << start << " " << m.val.substr(dotLoc - 1) << " " << end << " " << dist << " foo\n";
+                os = toByteArr(dist, m.metadata);
+            }
+            for (int i = 0; i < os.size(); i++) oq.push_back(os[i]);
+        }
+        else oq.push_back(m);
+    }
+    return oq;
+}
+
+std::string serialize(std::vector<Node> derefed) {
+    std::string o = "";
+    for (int i = 0; i < derefed.size(); i++) {
+        int v;
+        if (isNumberLike(derefed[i])) {
+            v = decimalToInt(derefed[i].val);
+        }
+        else if (derefed[i].val.substr(0,4) == "PUSH") {
+            v = 95 + decimalToInt(derefed[i].val.substr(4));
+        }
+        else {
+            v = opcode(derefed[i].val);
+        }
+        o += std::string("0123456789abcdef").substr(v/16, 1);
+        o += std::string("0123456789abcdef").substr(v%16, 1);
+    }
+    return o;
+}
+
+int main() {
+    Node n = rewrite(parseSerpent("x = 2 + 3"));
+    cout << printAST(n) << " r\n";
+    programData c = compile(n);
+    cout << printTokens(c.code) << " c\n";
+    c = finalize(c);
+    cout << printTokens(c.code) << " f\n";
+    std::vector<Node> d = dereference(c);
+    cout << printTokens(d) << " d\n";
+    std::string s = serialize(d);
+    cout << s << " s\n";
+    cout << printTokens(compile(rewrite(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]"))).code) << "\n";
+    cout << printTokens(dereference(finalize(compile(rewrite(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]")))))) << "\n";
+    cout << serialize(dereference(finalize(compile(rewrite(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]")))))) << "\n";
+}
---
+++ rewriter.cpp
@@ -0,0 +1,339 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include "parser.cpp"
+
+std::string macros[][2] = {
+    {
+        "(if @cond @do (else @else))",
+        "(if @cond @do @else)"
+    },
+    {
+        "(code @code)",
+        "@code"
+    },
+    {
+        "(access msg.data @ind)",
+        "(CALLDATALOAD (MUL 32 @ind))"
+    },
+    {
+        "(array @len)",
+        "(alloc (MUL 32 @len))"
+    },
+    {
+        "(while @cond @do)",
+        "(until (NOT @cond) @do)",
+    },
+    {
+        "(while (NOT @cond) @do)",
+        "(until @cond @do)",
+    },
+    {
+        "(if @cond @do)",
+        "(unless (NOT @cond) @do)",
+    },
+    {
+        "(if (NOT @cond) @do)",
+        "(unless @cond @do)",
+    },
+    {
+        "(access contract.storage @ind)",
+        "(SLOAD @ind)"
+    },
+    {
+        "(access @var @ind)",
+        "(MLOAD (ADD @var (MUL 32 @ind)))"
+    },
+    {
+        "(set (access contract.storage @ind) @val)",
+        "(SSTORE @ind @val)"
+    },
+    {
+        "(set (access @var @ind) @val)",
+        "(MSTORE (ADD @var (MUL 32 @ind)) @val)"
+    },
+    {
+        "(getch @var @ind)",
+        "(MOD (MLOAD (ADD @var @ind)) 256)"
+    },
+    {
+        "(setch @var @ind @val)",
+        "(MSTORE8 (ADD @var @ind) @val)",
+    },
+    {
+        "(send @to @value)",
+        "(call (SUB (GAS) 25) @to @value 0 0 0 0)"
+    },
+    {
+        "(send @gas @to @value)",
+        "(call @gas @to @value 0 0 0 0)"
+    },
+    {
+        "(sha3 @x)",
+        "(seq (set @1 @x) (SHA3 (ref @1) 32))"
+    },
+    {
+        "(sha3 @start @len)",
+        "(SHA3 @start @len)"
+    },
+    {
+        "(calldataload @start @len)",
+        "(CALLDATALOAD @start (MUL 32 @len))"
+    },
+    {
+        "(id @0)",
+        "@0"
+    },
+    {
+        "(return @x)",
+        "(seq (set @1 @x) (RETURN (ref @1) 32))"
+    },
+    {
+        "(return @start @len)",
+        "(RETURN @start (MUL 32 @len))"
+    },
+    {
+        "(&& @x @y)",
+        "(if @x @y 0)"
+    },
+    {
+        "(|| @x @y)",
+        "(seq (set @1 @x) (if (get @1) (get @1) @y))"
+    },
+    {
+        "(>= @x @y)",
+        "(NOT (LT @x @y))"
+    },
+    {
+        "(<= @x @y)",
+        "(NOT (GT @x @y))"
+    },
+    {
+        "(create @endowment @code)",
+        "(CREATE @endowment (ref @1) (lll (outer @code) (ref @1)))"
+    },
+    {
+        "(msg @gas @to @val @dataval)",
+        "(seq (set @1 @dataval) (CALL @gas @to @val (ref @1) 32 (ref @2) 32) (get @2))"
+    },
+    {
+        "(call @f @dataval)",
+        "(msg (SUB (GAS) 45) @f 0 @dataval)"
+    },
+    {
+        "(msg @gas @to @val @inp @inpsz)",
+        "(seq (CALL @gas @to @val @inp (MUL 32 @inpsz) (ref @1) 32) (get @1))"
+    },
+    {
+        "(call @f @inp @inpsz)",
+        "(seq (set @1 @inpsz) (msg (SUB (GAS) (ADD 25 (get @1))) @f 0 @inp (get @1)))"
+    },
+    {
+        "(msg @gas @to @val @inp @inpsz @outsz)",
+        "(seq (set @1 (MUL 32 @outsz)) (set @2 (alloc (get @1))) (POP (CALL @gas @to @val @inp (MUL 32 @inpsz) (ref @2) (get @1))) (get @2))"
+    },
+    {
+        "(outer (init @init @code))",
+        "(seq @init (RETURN 0 (lll @code 0)))"
+    },
+    {
+        "(outer (shared @shared @init @code))",
+        "(seq @shared @init (RETURN 0 (lll (seq @shared @code) 0)))"
+    },
+    {
+        "(outer @code)",
+        "(outer (init (seq) @code))"
+    },
+    {
+        "(seq (seq) @x)",
+        "@x"
+    },
+    { "msg.datasize", "(DIV (CALLDATASIZE) 32)" },
+    { "msg.sender", "(CALLER)" },
+    { "msg.value", "(CALLVALUE)" },
+    { "tx.gasprice", "(GASPRICE)" },
+    { "tx.origin", "(ORIGIN)" },
+    { "tx.gas", "(GAS)" },
+    { "contract.balance", "(BALANCE)" },
+    { "contract.address", "(ADDRESS)" },
+    { "block.prevhash", "(PREVHASH)" },
+    { "block.coinbase", "(COINBASE)" },
+    { "block.timestamp", "(TIMESTAMP)" },
+    { "block.number", "(NUMBER)" },
+    { "block.difficulty", "(DIFFICULTY)" },
+    { "block.gaslimit", "(GASLIMIT)" },
+    { "stop", "(STOP)" },
+    { "---END---", "" } //Keep this line at the end of the list
+};
+
+std::string synonyms[][2] = {
+    { "|", "OR" },
+    { "or", "||" },
+    { "&", "AND" },
+    { "and", "&&" },
+    { "xor", "XOR" },
+    { "elif", "if" },
+    { "!", "NOT" },
+    { "not", "NOT" },
+    { "byte", "BYTE" },
+    { "string", "alloc" },
+    { "create", "CREATE" },
+    { "+", "ADD" },
+    { "-", "SUB" },
+    { "*", "MUL" },
+    { "/", "SDIV" },
+    { "^", "EXP" },
+    { "%", "SMOD" },
+    { "$/", "DIV" },
+    { "$%", "MOD" },
+    { "$<", "LT" },
+    { "$>", "GT" },
+    { "<", "SLT" },
+    { ">", "SGT" },
+    { "=", "set" },
+    { "==", "EQ" },
+    { "---END---", "" } //Keep this line at the end of the list
+};
+
+struct matchResult {
+    std::map<std::string, Node> map;
+    bool success;
+};
+
+matchResult match(Node p, Node n) {
+    matchResult o;
+    o.success = false;
+    if (p.type == TOKEN) {
+        if (p.val == n.val) o.success = true;
+        else if (p.val[0] == '@') {
+            o.success = true;
+            o.map[p.val.substr(1)] = n;
+        }
+        return o;
+    }
+    else if (n.type==TOKEN || p.val!=n.val || p.args.size()!=n.args.size()) {
+        return o;
+    }
+    else {
+        for (int i = 0; i < p.args.size(); i++) {
+            matchResult oPrime = match(p.args[i], n.args[i]);
+            if (!oPrime.success) {
+                o.success = false;
+                return o;
+            }
+            for (std::map<std::string, Node>::iterator it = oPrime.map.begin();
+                 it != oPrime.map.end();
+                 it++) {
+                o.map[(*it).first] = (*it).second;
+            }
+        }
+        o.success = true;
+    }
+    return o;
+}
+
+Node subst(Node p,
+           std::map<std::string, Node> dict,
+           std::string varflag,
+           Metadata metadata) {
+    if (p.type == TOKEN && p.val[0] == '@') {
+        if (dict.count(p.val.substr(1))) return dict[p.val.substr(1)];
+        else return token(varflag + p.val.substr(1), metadata);
+    }
+    else if (p.type == TOKEN) {
+        return p;
+    }
+    else {
+        std::vector<Node> args;
+        for (int i = 0; i < p.args.size(); i++) {
+            args.push_back(subst(p.args[i], dict, varflag, metadata));
+        }
+        return astnode(p.val, args, p.metadata);
+    }
+}
+
+bool untouchable(Node node) {
+    return node.val == "set" || node.val == "ref" || node.val == "get";
+}
+
+
+Node apply_rules(Node node) {
+    int pos = 0;
+    std::string prefix = "_temp"+intToDecimal(counter)+"_";
+    counter += 1;
+    while(1) {
+        if (synonyms[pos][0] == "---END---") {
+            break;
+        }
+        else if (node.type == ASTNODE && node.val == synonyms[pos][0]) {
+            node.val = synonyms[pos][1];
+        }
+        pos++;
+    }
+    pos = 0;
+    bool done = false;
+    while(1) {
+        if (macros[pos][0] == "---END---") {
+            if (!done) break;
+            else {
+                pos = 0;
+                done = false;
+            }
+        }
+        Node pattern = parseLLL(macros[pos][0]);
+        matchResult mr = match(pattern, node);
+        if (mr.success) {
+            Node pattern2 = parseLLL(macros[pos][1]);
+            node = subst(pattern2, mr.map, prefix, node.metadata);
+            done = true;
+        }
+        pos++;
+    }
+    if (node.type == ASTNODE && node.val != "ref" && node.val != "get") {
+        int i = 0;
+        if (node.val == "set") i = 1;
+        for (i = i; i < node.args.size(); i++) {
+            node.args[i] = apply_rules(node.args[i]);
+        }
+    }
+    else if (node.type == TOKEN && !isNumberLike(node)) {
+        std::vector<Node> args;
+        args.push_back(node);
+        node = astnode("get", args, node.metadata);
+    }
+    return node;
+}
+
+Node preprocess(Node inp) {
+    std::vector<Node> args;
+    args.push_back(inp);
+    return astnode("outer", args, inp.metadata);
+}
+
+Node rewrite(Node inp) {
+    return apply_rules(preprocess(inp));
+}
+
+using namespace std;
+
+/*int main() {
+    cout << printAST(rewrite(parseSerpent("x = 2 + 3"))) << "\n";
+    cout << printAST(rewrite(parseSerpent("x = 2 + 3\ny = 5 * 7"))) << "\n";
+    cout << printAST(rewrite(parseSerpent("if x < 2 + 3:\n    y = 5 * 7"))) << "\n";
+    cout << printAST(rewrite(parseSerpent("if x < 2 + 3:\n    y = 5 * 7\nelif x < 9:\n    y = 15"))) << "\n";
+    cout << printAST(parseSerpent("if x < 2 + 3:\n    y = 5 * 7\nelif x < 9:\n    y = 15\nelif x < f(4):\n    y = -4\nelse:\n    y = 0")) << "\n";
+    cout << printAST(rewrite(parseSerpent("if x < 2 + 3:\n    y = 5 * 7\nelif x < 9:\n    y = 15\nelif x < f(4):\n    y = -4\nelse:\n    y = 0"))) << "\n";
+    cout << printAST(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]")) << "\n";
+    cout << printAST(rewrite(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]"))) << "\n";
+    cout << printAST(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]\n    return(1)\nelse:\n    return(0)")) << "\n";
+    cout << printAST(rewrite(parseSerpent("if contract.storage[msg.data[0]]:\n    contract.storage[msg.data[0]] = msg.data[1]\n    return(1)\nelse:\n    return(0)")));
+    cout << decimalAdd("256", "256") << "\n";
+    cout << decimalDigitMul("19683", 3) << "\n";
+    cout << decimalMul("65536", "65536") << "\n";
+    cout << decimalSub("65536", "32768") << "\n";
+    cout << decimalSub("1000000", "43") << "\n";
+    cout << decimalDiv("531441", "243") << "\n";
+    cout << decimalDiv("531441", "9") << "\n";
+    cout << decimalDiv("999999", "27") << "\n";
+}*/
