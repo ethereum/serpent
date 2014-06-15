f1eb0631b81c437447bc8876c8f52f0d7d85f043
Vitalik Buterin <v@buterin.com>
2014-06-15 15:24:08 -0400

    Added darwin13

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm Makefile
-if [[ "$OSTYPE" == 'darwin' ]]; then
+rm -f Makefile
+if [[ "$OSTYPE" == 'darwin' || "$OSTYPE" == 'darwin13' ]]; then
     echo 'PLATFORM_OPTS = ' >> Makefile
     cat Makefile.in | tail -n `expr $(cat Makefile.in | wc -l) - 1` >> Makefile
 else
