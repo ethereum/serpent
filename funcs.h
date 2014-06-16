87e5d816be889c90c84a935027407f4ee20b5b35
Vitalik Buterin <v@buterin.com>
2014-06-16 13:24:03 -0400

    Added function listing to funcs.h

--- funcs.h
+++ funcs.h
@@ -9,6 +9,19 @@
 #include "rewriter.h"
 #include "tokenize.h"
 
+// Function listing:
+//
+// parseSerpent      (serpent -> AST)      std::string -> Node
+// parseLLL          (LLL -> AST)          std::string -> Node
+// rewrite           (apply rewrite rules) Node -> Node
+// compileToLLL      (serpent -> LLL)      std::string -> Node
+// compileLLL        (LLL -> EVMhex)       Node -> std::string
+// prettyCompileLLL  (LLL -> EVMasm)       Node -> std::vector<Node>
+// prettyCompile     (serpent -> EVMasm)   std::string -> std::vector>Node>
+// compile           (serpent -> EVMhex)   std::string -> std::string
+// get_file_contents (filename -> file)    std::string -> std::string
+// exists            (does file exist?)    std::string -> bool
+
 Node compileToLLL(std::string input, std::string inputFile="");
 
 std::string compile(std::string input, std::string inputFile="");
