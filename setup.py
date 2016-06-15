0e5c6817b3d824339e9598edfc520c8857e28a8b
Piper Merriam <pipermerriam@gmail.com>
2016-06-15 12:01:35 -0600

    add python version classifiers to setup.py

--- setup.py
+++ setup.py
@@ -43,5 +43,13 @@ setup(
         'console_scripts': [
             'serpent = serpent:main',
         ],
-    }
-    ),
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
