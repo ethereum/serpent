bc4ce591c11eed7ca8e6491095535dd0fc00df28
Piper Merriam <pipermerriam@gmail.com>
2016-06-17 10:35:00 -0600

    fix setup.py and dox errors

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
 )
